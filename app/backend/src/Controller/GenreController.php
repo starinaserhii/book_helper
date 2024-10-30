050d54ac33a7c2a3e3fc17e967c47694c7bb9b8e
starina <starinasergey@gmail.com>
2024-10-30 16:52:09 +0200

    add rout genre

--- app/backend/src/Controller/GenreController.php
+++ app/backend/src/Controller/GenreController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Genre;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,8 +12,20 @@ use Symfony\Component\HttpFoundation\Request;
 class GenreController extends AbstractController
 {
     #[Route('/api/genre/add', name: 'app_genre')]
-    public function index(Request $request): JsonResponse
+    public function index(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
+        $name = $request->request->get('name');
+        $entity = new Genre();
+        $entity->setName($name);
+
+        $repository = $entityManager->getRepository(Genre::class);
+        $genres = $repository->findOneBy(['name' => $name]);
+
+        if (empty($genres)) {
+            $entityManager->persist($entity);
+            $entityManager->flush();
+        }
+
         return new JsonResponse(['success' => true]);
     }
 }
